979657442cca547fb096c67c20bb607b6a190918
Zekna <zsharpsquared@gmail.com>
2015-04-27 11:21:56 -0700

    initial commit

---
+++ assert_tests.py
@@ -0,0 +1,64 @@
+from asserts import (
+    typecheck,
+    validate,
+    ValidationError,
+    ValidationResult,
+)
+
+import unittest
+
+
+#----------------------
+# Test fodder
+#----------------------
+
+@typecheck
+def type_checked(best: int, slot: str) -> str:
+    return slot * best
+
+
+@typecheck
+def type_checked_bad_return(a: int) -> int:
+    return str(a)
+
+
+@typecheck
+def custom_validator(a: int) -> ValidationResult:
+    if a < 5:
+        return ValidationResult(True, None)
+    else:
+        return ValidationResult(False, "value was larger than 5.")
+
+
+@validate
+def validated(a: custom_validator) -> custom_validator:
+    return a + 5
+
+
+#----------------------
+# Tests
+#----------------------
+
+class TypecheckTestCase(unittest.TestCase):
+    def test_check_pass(self):
+        type_checked(2, "wut ")
+
+    def test_check_args_fail(self):
+        self.assertRaises(TypeError, type_checked, "5", "wut ")
+        self.assertRaises(TypeError, type_checked, 5, 20)
+
+    def test_check_result_fails(self):
+        self.assertRaises(TypeError, type_checked_bad_return, 2)
+
+    def test_valid_pass(self):
+        validated(-1)
+
+    def test_valid_arg_fails(self):
+        self.assertRaises(ValidationError, validated, 6)
+
+    def test_valid_result_fails(self):
+        self.assertRaises(ValidationError, validated, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ asserts.py
@@ -0,0 +1,61 @@
+import functools
+from collections import namedtuple
+
+
+# ------------------
+# type check
+# ------------------
+
+
+def typecheck(f):
+    return _core_wrapper(f, _compare_types)
+
+
+def _compare_types(f, name, arg):
+    expected = f.__annotations__.get(name, None)
+    if expected and not isinstance(arg, expected):
+        raise TypeError("{} was expected to be of type {}, not {}".format(arg, expected, type(arg)))
+
+
+# -------------------
+# custom validation
+# -------------------
+
+
+def validate(f):
+    return _core_wrapper(f, _run_validator)
+
+
+ValidationResult = namedtuple('ValidationResult', 'result reason')
+
+
+class ValidationError(BaseException):
+    pass
+
+
+def _run_validator(f, name, arg):
+    validator = f.__annotations__.get(name, None)
+    result, reason = validator(arg)
+    if validator and not result:
+        raise ValidationError("arg {} did not pass validation: {}".format(result, reason))
+
+
+#---------------------
+# core
+#---------------------
+
+
+def _core_wrapper(f, compare_f):
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        for i, arg in enumerate(args[:f.__code__.co_nlocals]):
+            compare_f(f, f.__code__.co_varnames[i], arg)
+        for name, arg in kwargs.items():
+            compare_f(f, name, arg)
+
+        result = f(*args, **kwargs)
+
+        compare_f(f, 'return', result)
+        return result
+    return wrapper
