804be05b3686da0e3bf37f353194198a45cfa754
Zekna <zsharpsquared@gmail.com>
2015-07-10 14:47:29 -0700

    setup.{py,cfg};restructure to be an actual package
    Set up everything to be a real python package whoo

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import (
+    setup,
+    find_packages,
+)
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md")) as f:
+    long_description = f.read()
+
+
+setup(
+    name="py_types",
+    version="0.1.0.dev1",
+    description="Gradual typing for python 3.",
+    long_description=long_description,
+    url="https://github.com/zekna/py-types",
+    author="Zach Nelson",
+    author_email="kzacharynelson@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Develpoment Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Tools",
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+    keywords="type checking development schema",
+    packages=find_packages(exclude=["tests*"]),
+
+    install_requires=[],
+    extras_require={},
+    package_data={},
+    data_files=[],
+    entry_points={},
+)
